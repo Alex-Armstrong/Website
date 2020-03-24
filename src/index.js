25f46392ad8073628778a0b15356cafa4505a7d8
Alex-Armstrong <62529735+Alex-Armstrong@users.noreply.github.com>
2020-03-23 17:17:05 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import "./styles.css";
+
+const pages = {
+  bio: {
+    url: "/bio",
+    header: "Hi! I'm Alex"
+  },
+  pics: {
+    url: "/pics",
+    header: "Here are some dope pics I took"
+  },
+  index: {
+    url: "/",
+    header: "Welcome to this sick ass site!"
+  }
+};
+
+const getPageInfo = path => {
+  const DefaultPage = "index";
+  let page = Object.keys(pages).find(pageName => {
+    return pages[pageName].url === path;
+  });
+
+  if (!page) {
+    page = DefaultPage;
+  }
+
+  return pages[page];
+};
+// const page = "pics";
+const path = window.location.pathname;
+const page = getPageInfo(path);
+
+document.getElementById("app").innerHTML = ` 
+<h1>${page.header}</h1>
+<div>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+  <br/><br/>path: "${path}"
+</div>
+`;
